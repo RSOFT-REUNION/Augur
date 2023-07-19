6dc941695967da0ec136879c94bc463ac7f150f2
HOARAU BRIAN (RSOFT) <brian@rsoft.re>
2023-07-19 15:22:59 +0400

    ✅ Ajout de la page de visualisation des produits ✅ La recherche des produits redirige maintenant correctement sur le produit concernée

--- resources/views/pages/frontend/products/product-single.blade.php
+++ resources/views/pages/frontend/products/product-single.blade.php
@@ -1,5 +1,34 @@
 @extends('layouts.frontend')
 
-@section()
-
+@section('content-template')
+    <div class="container mx-auto">
+        <div class="">
+            <a href="{{ route('fo.products') }}" class="btn-filled_secondary"><i class="fa-solid fa-arrow-left"></i></a>
+        </div>
+        <div class="flex items-center">
+            <div class="flex-1">
+                <div class="force-center">
+                    <img src="{{ asset('storage/products/'. $product->picture) }}" width="500px"/>
+                </div>
+            </div>
+            <div class="flex-1">
+                <h1 class="text-2xl font-bold">{{ $product->title }}</h1>
+                <p>{{ $product->description }}</p>
+                <div class="mt-5">
+                    <h2 class="text-primary font-bold text-xl">Les labels</h2>
+                    <div class="inline-flex items-center">
+                        @foreach($product->getLabels() as $lab)
+                            @foreach($labels as $label)
+                                @if($lab == $label->title)
+                                    <object data="{{ asset('storage/medias/'. $label->getPicture()) }}" width="100px"></object>
+                                @else
+                                    <span class="text-sm bg-gray-100 px-2 py-1 rounded-lg mr-2">{{ $lab }}</span>
+                                @endif
+                            @endforeach
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
