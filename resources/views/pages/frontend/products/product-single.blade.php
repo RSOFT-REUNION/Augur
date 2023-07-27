2f23547df8d080d74e7636aa4aa80e2ad942139c
HOARAU BRIAN (RSOFT) <brian@rsoft.re>
2023-07-27 09:58:53 +0400

    ✅Modification du product single

--- resources/views/pages/frontend/products/product-single.blade.php
+++ resources/views/pages/frontend/products/product-single.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.frontend')
 
 @section('content-template')
-    <div class="container mx-auto">
+    <div class="container mx-auto py-10">
         <div class="">
             <a href="{{ route('fo.products') }}" class="btn-filled_secondary"><i class="fa-solid fa-arrow-left"></i></a>
         </div>
@@ -11,25 +11,22 @@
                     <img src="{{ asset('storage/products/'. $product->picture) }}" width="500px"/>
                 </div>
             </div>
-            <div class="flex-2">
-                <div class="border-solid flex flex-col gap-8 h-[315px] shrink-0 border-[#E8E8E8] border rounded-lg">
-                    <div class="bg-[#E8E8E8] flex flex-col mb-1 items-start pt-1 pb-2 px-4 rounded-tl-lg rounded-tr-lg">
-                        <div class="whitespace-nowrap text-4xl font-['Roboto_Condensed'] font-medium leading-[52px] w-1/2">
-                            {{ $product->title }}
+            <div class="flex-none">
+                <div class="border flex flex-col shrink-0 border-gray-100 rounded-lg min-w-[700px]">
+                    <div class="bg-gray-100 flex flex-col mb-1 pt-1 pb-2 px-4 rounded-tl-lg rounded-tr-lg">
+                        <div class="whitespace-nowrap text-4xl font-['Roboto_Condensed'] font-medium leading-[52px] text-center">
+                            <h2>{{ $product->title }}</h2>
                         </div>
                     </div>
-                @foreach ($product->getLabelLink() as $product_label)
-                    <div class="flex gap-2 flex-wrap items-center ml-20 mr-24">
-                        <a href="{{ route('fo.label', ['slug' => $product_label->slug]) }}"><img src="{{ asset('storage/medias/'. $product_label->getPicture()) }}" width="100px"/></a>
-                    </div>
-                    <div class="flex flex-row items-center ml-12 mr-16">
-                        <div class="relative flex flex-col w-[351px] items-end">
-                            <div class="text-center w-1/2 font-['Korolev'] font-medium leading-[16px] text-[#1b2e47] absolute top-0 left-0 h-4">
-                                {{ $product_label->labels }}
-                            </div>
+                    @if(count($product->getLabelLink()) > 0)
+                        <div class="flex items-center mx-auto gap-5 py-5">
+                            @foreach ($product->getLabelLink() as $product_label)
+                                <a href="{{ route('fo.label', ['slug' => $product_label->slug]) }}"><img src="{{ asset('storage/medias/'. $product_label->getPicture()) }}" style="height: 100px"/></a>
+                            @endforeach
                         </div>
-                    </div>
-                @endforeach
+                    @else
+                        <p class="text-gray-500 my-5 text-center">Aucun label n'est lié à ce produit</p>
+                    @endif
             </div>
         </div>
     </div>
