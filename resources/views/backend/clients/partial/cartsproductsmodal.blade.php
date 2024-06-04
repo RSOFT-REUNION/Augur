3816afbaaabc34978ef940dcd3114837c8f7588e
VIENNE Ludovic <139841836+LudovicRsoft@users.noreply.github.com>
2024-06-04 14:57:13 +0400

    Correction du panier (Backend)

--- resources/views/backend/clients/partial/cartsproductsmodal.blade.php
+++ resources/views/backend/clients/partial/cartsproductsmodal.blade.php
@@ -11,7 +11,7 @@
                         <div class="card-body p-4">
                             <div class="row d-flex justify-content-between align-items-center">
                                 <div class="col-md-2 col-lg-2 col-xl-2">
-                                    {{ $product->getFirstImages($product) }}
+                                    <img src="{{ getImageUrl(removeStorageFromURL($product->fav_image), 200, 200, 'fill-max') }}" class="img-fluid" alt="{{ $product->name }}">
                                 </div>
                                 <div class="col-md-3 col-lg-3 col-xl-3">
                                     <p class="lead fw-normal mb-2">{{ getProductInfos($product->product_id)->name  }}</p>
@@ -20,7 +20,7 @@
                                     Quantité : {{$product->quantity}}
                                 </div>
                                 <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
-                                    <h5 class="mb-0">{{ $product->price_ttc }} €</h5>
+                                    <h5 class="mb-0">{{ formatPriceToFloat($product->price_ttc) }} €</h5>
                                 </div>
                             </div>
                         </div>
