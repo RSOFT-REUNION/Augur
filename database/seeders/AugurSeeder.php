78fcf7e04fd11b450eb22e782b6738839cfc2994
VIENNE Ludovic <139841836+LudovicRsoft@users.noreply.github.com>
2024-06-13 14:36:04 +0400

    Mise a jour du seeder pour créer les categories

--- database/seeders/AugurSeeder.php
+++ database/seeders/AugurSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Catalog\Category;
 use App\Models\Orders\Delivery;
 use Illuminate\Database\Seeder;
 
@@ -20,6 +21,34 @@ class AugurSeeder extends Seeder
             'description' => 'Livraison a domicile',
             'image' => 'livexpress.png',
             'price_ttc' => 10
+        ]);
+        Category::create([
+            'name' => 'Produits surgelés',
+            'slug' => 'produits-surgeles',
+            'image' => 'produits.surgeles.jpg',
+            'active' => 1,
+            'is_menu' => 1,
+        ]);
+        Category::create([
+            'name' => 'Epicerie fine',
+            'slug' => 'epicerie-fine',
+            'image' => 'epicerie.fine.jpg',
+            'active' => 1,
+            'is_menu' => 1,
+        ]);
+        Category::create([
+            'name' => 'Produits Vrac',
+            'slug' => 'produits-vrac',
+            'image' => 'produits.vrac.jpg',
+            'active' => 1,
+            'is_menu' => 1,
+        ]);
+        Category::create([
+            'name' => 'Produits Péi',
+            'slug' => '	produits-pei',
+            'image' => 'produits.pei.jpg',
+            'active' => 1,
+            'is_menu' => 1,
         ]);
     }
 }
