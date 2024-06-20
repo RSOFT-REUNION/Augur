0a5912d75e1d52b673c7e3d2a1dc52d129829679
DevRsoft (RSOFT) <dev@rsoft.re>
2024-06-20 15:35:25 +0400

    orthographe

--- resources/views/frontend/orders/validated.blade.php
+++ resources/views/frontend/orders/validated.blade.php
@@ -7,13 +7,13 @@
         <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
             <ol class="breadcrumb breadcrumb-nav mt-5 p-3 rounded-3 shadow">
                 <li class="breadcrumb-item"><a href="{{ route('index') }}"><i class="fa-solid fa-home"></i></a></li>
-                <li class="breadcrumb-item active text-white" aria-current="page">Commande valider</li>
+                <li class="breadcrumb-item active text-white" aria-current="page">Commande validé</li>
             </ol>
         </nav>
     </div>
 
-    <h2 class="text-center mb-5">Votre commande est valider, elle sera traiter dans les plus bref delais.</h2>
-    <h3 class="text-center mb-5">Merci d'avoir passer votre commande</h3>
+    <h2 class="text-center mb-5">Votre commande est validée, elle sera traitée dans les plus brefs délais.</h2>
+    <h3 class="text-center mb-5">Merci d'avoir passé votre commande chez Aügur!</h3>
 
     <div class="text-center">
         <a class="hvr-grow-shadow btn btn-warning btn-lg mb-2" href="{{ route('orders.show') }}"><i class="fa-solid fa-basket-shopping"></i> Mes commandes</a>
