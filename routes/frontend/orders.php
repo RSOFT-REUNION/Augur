62efc3a819d7d507e08dfc9fe40e67f9351634d5
DevRsoft (RSOFT) <dev@rsoft.re>
2024-06-20 15:35:34 +0400

    fix route orders en get

--- routes/frontend/orders.php
+++ routes/frontend/orders.php
@@ -9,5 +9,5 @@ $slugRegex = '[0-9a-z\-/]+';
 //Route::post('/cart_validation', [OrdersController::class, 'cart_validation'])->name('orders.cart_validation');
 Route::post('/send_payment', [OrdersController::class, 'sendPaymentRequest'])->name('orders.send_payment');
 Route::post('/return_payment', [OrdersController::class, 'getPaymentReturn'])->name('orders.return_payment');
-Route::post('/payment_success', [OrdersController::class, 'orderValidated'])->name('orders.success');
-Route::post('/payment_failed', [OrdersController::class, 'orderFailed'])->name('orders.failed');
+Route::get('/payment_success', [OrdersController::class, 'orderValidated'])->name('orders.success');
+Route::get('/payment_failed', [OrdersController::class, 'orderFailed'])->name('orders.failed');
